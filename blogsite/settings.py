c3b4aede42c3dfd43ee73e8c57b6f5db5b825bed
Sambeth <sambethslim@gmail.com>
2017-06-17 15:50:25 +0100

    create view and url link and html template

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$-zzme@z8q(&u=g5lb-%&6$bwh)a$h&5ch1^d6kq47f+l74%u-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pearl123.pythonanywhere.com ']
+ALLOWED_HOSTS = ['127.0.0.1', 'pearl123.pythonanywhere.com']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'blogsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- post/views.py
+++ post/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    context = {
+        'title': 'Emefa',
+    }
+    template = 'post/home.html'
+    return render(request, template, context)
